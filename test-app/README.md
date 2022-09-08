5ee10664951e43f5fce0c76be283c3e7fe4a54b9
Daniel Carlsson <dc222bz@student.lnu.se>
2022-09-08 23:00:40 +0200

    Init file structure

---
+++ test-app/README.md
@@ -0,0 +1,3 @@
+# 1DV610-Laboration0
+First Laboration in course 1DV610. <br>
+The application takes the input of a name as a string and presents through a matematical formula what color that string converts to. 
---
+++ test-app/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Daniel Carlsson <dc222bz@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import './components/my-name/index.js'
+
+document.querySelector('#container').appendChild(document.createElement('my-test'))
